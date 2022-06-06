907aad19244afcefc536483a59b37d0eb8613815
Tofikson <79985872+Tofikson@users.noreply.github.com>
2022-06-06 14:53:50 +0200

    change of plans, szortifuy tiem

---
+++ the-meme-generator/database.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace the_meme_generator
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<users> Users { get; set; }
+        public DbSet<playlista> Playlista { get; set; }
+        public DbSet<utwory> Utwory { get; set; }
+        public DbSet<wykonawcy> Wykonawcy { get; set; }
+        public DbSet<albumy> Albumy { get; set; }
+
+        public string DbPath { get; }
+        public DatabaseContext ()
+        {
+            var szrotifyDb = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(szrotifyDb);
+            DbPath = System.IO.Path.Join(path, "szrotifyDb.db");
+        }
+    }
+}
