dbe955f0d4007ea791cd9456b5df443209dd1728
Tofikson <79985872+Tofikson@users.noreply.github.com>
2022-06-06 15:11:07 +0200

    uzupelnianie kolumn bazy

--- the-meme-generator/database.cs
+++ the-meme-generator/database.cs
@@ -17,11 +17,56 @@ namespace the_meme_generator
         public DbSet<albumy> Albumy { get; set; }
 
         public string DbPath { get; }
-        public DatabaseContext ()
+        public DatabaseContext()
         {
             var szrotifyDb = Environment.SpecialFolder.LocalApplicationData;
             var path = Environment.GetFolderPath(szrotifyDb);
             DbPath = System.IO.Path.Join(path, "szrotifyDb.db");
+
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            Console.WriteLine(DbPath);
+            dbContextOptionsBuilder.UseSqlite($"DataSource={DbPath}");
+        }
+    }
+
+
+    public class users
+    {
+        public int id_user { get; set; }
+        public string login { get; set; }
+        public string haslo { get; set; }
+    }
+    public class playlista
+    {
+        public int id_playlisty { get; set; }
+        public int id_user { get; set; }
+        public string nazwa_playlisty { get; set; }
+        public int id_utworu { get; set; }
+    }
+    public class utwory
+    {
+        public int id_utworu { get; set; }
+        public int id_wykonawcy { get; set; }
+        public int id_albumu { get; set; }
+        public string tytul { get; set; }
+        public string czas_utworu { get; set; }
+        public DateTime data_dodania { get; set; }
+        public int lista_odtworzen { get; set; }
+    }
+    public class wykonawcy
+    {
+        public int id_wykonawcy { get; set; }
+        public string wykonawca { get; set; }
+        public DateTime rozpoczecie_kariery { get; set; }
+        public int wydanych_albumow { get; set; }
+    }
+    public class albumy
+    {
+        public int id_albumu { get; set; }
+        public string album { get; set; }
+        public DateTime data_wydania { get; set; }
+        public int liczba_utworow { get; set; }
     }
 }
