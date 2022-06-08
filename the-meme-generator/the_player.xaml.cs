067637cd70e3153ec4b9be99c3e23af861d98c07
Tofikson <79985872+Tofikson@users.noreply.github.com>
2022-06-08 19:27:28 +0200

    tak, player gra, ale tylko raz - why

--- the-meme-generator/the_player.xaml.cs
+++ the-meme-generator/the_player.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace the_meme_generator
 {
@@ -20,29 +24,68 @@ namespace the_meme_generator
     /// 
     public partial class the_player : Window
     {
+        private MediaPlayer mediaplayer = new MediaPlayer();
+        public string obecnyUtwor;
+
         public the_player()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e) // play/pause
-        {
-            //if (Playpause.Background == 
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e) // right skip
-        {
 
         }
+        void timerTick(object sender, EventArgs e)
+        {
+            if(mediaplayer.Source != null)
+            {
+                totalTime.Content = String.Format($"{mediaplayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss")}");
+                currentTime.Content = String.Format($"{mediaplayer.Position.ToString(@"mm\:ss")}");
+                title.Content = String.Format($"{obecnyUtwor}");
+            }
+            else
+            {
+                totalTime.Content = "0:00";
+                currentTime.Content = "0:00";
+                title.Content = "Nie wybrano utworu";
+            }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e) // left skip/ replay
+        }
+
+        private void Button_Play(object sender, RoutedEventArgs e) // play
+        {
+            mediaplayer.Play();
+        }
+
+        private void Button_Stop(object sender, RoutedEventArgs e) // pause
+        {
+            mediaplayer.Pause();
+        }
+        private void Button_Choose(object sender, RoutedEventArgs e) // right skip
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaplayer.Open(new Uri(openFileDialog.FileName));
+                obecnyUtwor = openFileDialog.FileName;
+            }
+                
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timerTick;
+            timer.Start();
+        }
+
+        private void Button_Next(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void Button_Click_3(object sender, RoutedEventArgs e) // speaker
+        private void Button_Rewind(object sender, RoutedEventArgs e)
         {
+            //if(speaker.)
+            //{
 
+            //}
         }
     }
 }
